dba4e0e6de0122093d9ba66613cde9d78337cd52
Alessandro Crugnola <crugnola@adobe.com>
2016-11-20 20:00:23 -0500

    gesture interfaces

---
+++ uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/UIDiscreteGestureRecognizer.java
@@ -0,0 +1,17 @@
+package it.sephiroth.android.library.uigestures;
+
+/**
+ * Created by Alessandro Crugnola on 11/20/16
+ * <p>
+ * Any {@link UIGestureRecognizer} that implements the
+ * UIDiscreteGestureRecognizer will only have these possible states:
+ * <ul>
+ * <li>{@link UIGestureRecognizer.State#Possible}</li>
+ * <li>{@link UIGestureRecognizer.State#Ended}</li>
+ * <li>{@link UIGestureRecognizer.State#Failed}</li>
+ * <li>{@link UIGestureRecognizer.State#Cancelled}</li>
+ * </ul>
+ */
+
+public interface UIDiscreteGestureRecognizer {
+}
