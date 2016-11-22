3447ce8e054065ed61b9389d75a3abe5055cbee7
Alessandro Crugnola <crugnola@adobe.com>
2016-11-22 16:10:43 -0500

    pinch gesture

---
+++ uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/UIPinchGestureRecognizer.java
@@ -0,0 +1,244 @@
+package it.sephiroth.android.library.uigestures;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.MotionEvent;
+
+/**
+ * Created by alessandro crugnola on 11/22/16.
+ * UIGestureRecognizer
+ * <p>
+ * UIPinchGestureRecognizer is a subclass of UIGestureRecognizer that looks for pinching gestures involving two touches.
+ * When the user moves the two fingers toward each other, the conventional meaning is zoom-out; when the user moves the two
+ * fingers away from each other, the conventional meaning is zoom-in.
+ */
+
+@SuppressWarnings ("unused")
+public class UIPinchGestureRecognizer extends UIGestureRecognizer
+    implements UIContinuousRecognizer, ScaleGestureDetector.OnScaleGestureListener {
+
+    private static final int MESSAGE_RESET = 1;
+    private final ScaleGestureDetector mScaleGestureDetector;
+    private float mTotalScale;
+    private boolean mFireEvents;
+
+    @Override
+    protected void handleMessage(final Message msg) {
+        switch (msg.what) {
+            case MESSAGE_RESET:
+                setState(State.Possible);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public UIPinchGestureRecognizer(@Nullable final Context context) {
+        super(context);
+        mScaleGestureDetector = new ScaleGestureDetector(context, this);
+        setQuickScaleEnabled(false);
+        mFireEvents = false;
+    }
+
+    /**
+     * Set whether the associated {@link ScaleGestureDetector.OnScaleGestureListener} should receive onScale callbacks
+     * when the user performs a doubleTap followed by a swipe. Note that this is enabled by default
+     * if the app targets API 19 and newer.<br/>
+     * Default value is false.
+     *
+     * @param enabled true to enable quick scaling, false to disable
+     * @since 1.0.0
+     */
+    public void setQuickScaleEnabled(boolean enabled) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            mScaleGestureDetector.setQuickScaleEnabled(enabled);
+        }
+    }
+
+    /**
+     * @param enabled enable/disable stylus scaling. Note: it is only available for android 23 and above
+     * @since 1.0.0
+     */
+    @RequiresApi (api = Build.VERSION_CODES.M)
+    public void setStylusScaleEnabled(boolean enabled) {
+        mScaleGestureDetector.setStylusScaleEnabled(enabled);
+    }
+
+    @Override
+    protected boolean onTouchEvent(MotionEvent ev) {
+        if (isEnabled()) {
+            mScaleGestureDetector.onTouchEvent(ev);
+            return getCancelsTouchesInView();
+        }
+        return false;
+    }
+
+    @Override
+    public int getNumberOfTouches() {
+        return mScaleGestureDetector.getNumberOfTouches();
+    }
+
+    /**
+     * @return The total scale factor since the gesture began
+     */
+    public float getScale() {
+        return mTotalScale;
+    }
+
+    /**
+     * Returns the current scale factor
+     *
+     * @return
+     * @since 1.0.0
+     */
+    public float getScaleFactor() {
+        return mScaleGestureDetector.getScaleFactor();
+    }
+
+    @Override
+    public void onStateChanged(@NonNull final UIGestureRecognizer recognizer) {
+        logMessage(Log.VERBOSE, "onStateChanged(%s, %s)", recognizer, recognizer.getState());
+        logMessage(Log.VERBOSE, "state: %s", getState());
+
+        if (recognizer.getState() == State.Failed && getState() == State.Began) {
+            mFireEvents = true;
+            stopListenForOtherStateChanges();
+            fireActionEvent();
+
+        } else if (recognizer.inState(State.Began, State.Ended) && getState() == State.Possible) {
+            stopListenForOtherStateChanges();
+            removeMessages();
+            setState(State.Failed);
+        }
+    }
+
+    @Override
+    public boolean onScale(final ScaleGestureDetector scaleGestureDetector) {
+        if (isEnabled() && inState(State.Began, State.Changed)) {
+            mTotalScale += scaleGestureDetector.getScaleFactor() - 1;
+            if (getState() == State.Began) {
+                if (mFireEvents) {
+                    setState(State.Changed);
+                    fireActionEvent();
+                }
+            } else if (getState() == State.Changed) {
+                setState(State.Changed);
+                fireActionEvent();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(final ScaleGestureDetector scaleGestureDetector) {
+        setState(State.Possible);
+
+        if (isEnabled() && getState() == State.Possible) {
+            mTotalScale = scaleGestureDetector.getScaleFactor();
+            removeMessages(MESSAGE_RESET);
+
+            if (getDelegate().shouldBegin(this)) {
+                setState(State.Began);
+
+                if (null == getRequireFailureOf()) {
+                    mFireEvents = true;
+                    fireActionEvent();
+                } else {
+                    if (getRequireFailureOf().getState() == State.Failed) {
+                        mFireEvents = true;
+                        fireActionEvent();
+                    } else {
+                        listenForOtherStateChanges();
+                        mFireEvents = false;
+                        logMessage(Log.DEBUG, "waiting...");
+                    }
+                }
+            } else {
+                setState(State.Failed);
+            }
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(final ScaleGestureDetector scaleGestureDetector) {
+        if (inState(State.Began, State.Changed)) {
+            setState(State.Ended);
+            if (mFireEvents) {
+                fireActionEvent();
+            }
+        }
+    }
+
+    @Override
+    public float getCurrentLocationX() {
+        return mScaleGestureDetector.getFocusX();
+    }
+
+    @Override
+    public float getCurrentLocationY() {
+        return mScaleGestureDetector.getFocusY();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getCurrentSpan()
+     * @since 1.0.0
+     */
+    public float getCurrentSpan() {
+        return mScaleGestureDetector.getCurrentSpan();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getCurrentSpanX()
+     * @since 1.0.0
+     */
+    public float getCurrentSpanX() {
+        return mScaleGestureDetector.getCurrentSpanX();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getCurrentSpanY()
+     * @since 1.0.0
+     */
+    public float getCurrentSpanY() {
+        return mScaleGestureDetector.getCurrentSpanY();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getPreviousSpan()
+     * @since 1.0.0
+     */
+    public float getPreviousSpan() {
+        return mScaleGestureDetector.getPreviousSpan();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getPreviousSpanX()
+     * @since 1.0.0
+     */
+    public float getPreviousSpanX() {
+        return mScaleGestureDetector.getPreviousSpanX();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getPreviousSpanY()
+     * @since 1.0.0
+     */
+    public float getPreviousSpanY() {
+        return mScaleGestureDetector.getPreviousSpanY();
+    }
+
+    /**
+     * @see ScaleGestureDetector#getTimeDelta()
+     * @since 1.0.0
+     */
+    public long getTimeDelta() {
+        return mScaleGestureDetector.getTimeDelta();
+    }
+}
