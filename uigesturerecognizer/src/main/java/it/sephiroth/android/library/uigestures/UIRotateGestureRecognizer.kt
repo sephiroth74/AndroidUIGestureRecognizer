e9a203160fc4994ca5254d6cf08289e48623a954
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2018-12-02 08:58:58 -0500

    Update UIRotateGestureRecognizer.kt

--- uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/UIRotateGestureRecognizer.kt
+++ uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/UIRotateGestureRecognizer.kt
@@ -5,6 +5,7 @@ import android.graphics.PointF
 import android.os.Message
 import android.util.Log
 import android.view.MotionEvent
+import kotlin.math.atan2
 
 /**
  * UIRotationGestureRecognizer is a subclass of UIGestureRecognizer that looks for rotation gestures involving two
@@ -23,6 +24,7 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
      *
      * @since 1.0.0
      */
+    @Suppress("MemberVisibilityCanBePrivate")
     var rotationThreshold: Double = 0.toDouble()
 
     /**
@@ -32,31 +34,36 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
      * @see .getRotationInDegrees
      * @since 1.0.0
      */
-    var rotationInRadians: Float = 0.toFloat()
+    @Suppress("MemberVisibilityCanBePrivate")
+    var rotationInRadians: Double = 0.0
         private set
+
+    private var mInitialRotation: Double = 0.toDouble()
 
     private var x1: Float = 0.toFloat()
     private var y1: Float = 0.toFloat()
     private var x2: Float = 0.toFloat()
     private var y2: Float = 0.toFloat()
 
-    private var mPreviousAngle: Float = 0.toFloat()
+    private var mPreviousAngle: Double = 0.toDouble()
 
     /**
      * @return The velocity of the rotation gesture in radians per second.
      * @since 1.0.0
      */
-    var velocity: Float = 0.toFloat()
+    @Suppress("MemberVisibilityCanBePrivate")
+    var velocity: Double = 0.toDouble()
         private set
 
     private var mValid: Boolean = false
     private var mStarted: Boolean = false
+    private var mDown: Boolean = false
     private var mPtrID1: Int = 0
     private var mPtrID2: Int = 0
     private var mPreviousEvent: MotionEvent? = null
     private val mCurrentLocation = PointF()
 
-    override var numberOfTouches: Int = 0
+    final override var numberOfTouches: Int = 0
         internal set
 
     /**
@@ -66,14 +73,15 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
      * @see .getRotationInRadians
      * @since 1.0.0
      */
-    val rotationInDegrees: Float
+    @Suppress("unused")
+    val rotationInDegrees: Double
         get() {
-            var angle = Math.toDegrees(rotationInRadians.toDouble()).toFloat() % 360
-            if (angle < -180f) {
-                angle += 360.0f
+            var angle = Math.toDegrees(rotationInRadians) % 360
+            if (angle < -180) {
+                angle += 360.0
             }
-            if (angle > 180f) {
-                angle -= 360.0f
+            if (angle > 180) {
+                angle -= 360.0
             }
             return angle
         }
@@ -89,6 +97,7 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
             MESSAGE_RESET -> {
                 stopListenForOtherStateChanges()
                 state = UIGestureRecognizer.State.Possible
+                mInitialRotation = 0.0
                 setBeginFiringEvents(false)
             }
             else -> {
@@ -100,13 +109,12 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
         rotationThreshold = ROTATION_SLOP
         mPtrID1 = INVALID_POINTER_ID
         mPtrID2 = INVALID_POINTER_ID
-        velocity = 0f
+        velocity = 0.toDouble()
         numberOfTouches = 0
     }
 
     override fun onStateChanged(recognizer: UIGestureRecognizer) {
-        logMessage(Log.VERBOSE, "onStateChanged(%s, %s)", recognizer, recognizer.state?.name!!)
-        logMessage(Log.VERBOSE, "state: %s", state?.name!!)
+        logMessage(Log.VERBOSE, "onStateChanged(${recognizer.state?.name})")
 
         if (recognizer.state === UIGestureRecognizer.State.Failed && state === UIGestureRecognizer.State.Began) {
             stopListenForOtherStateChanges()
@@ -167,15 +175,20 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
 
         when (action and MotionEvent.ACTION_MASK) {
             MotionEvent.ACTION_DOWN -> {
+                if (!delegate?.shouldReceiveTouch?.invoke(this)!!) {
+                    return cancelsTouchesInView
+                }
                 mValid = false
                 mStarted = false
+                mDown = true
+                mInitialRotation = 0.toDouble()
                 stopListenForOtherStateChanges()
                 state = UIGestureRecognizer.State.Possible
                 setBeginFiringEvents(false)
             }
 
             MotionEvent.ACTION_POINTER_DOWN -> {
-                if (count == 2 && state !== UIGestureRecognizer.State.Failed) {
+                if (mDown && count == 2 && state != UIGestureRecognizer.State.Failed) {
                     mPtrID1 = event.getPointerId(0)
                     mPtrID2 = event.getPointerId(1)
 
@@ -188,14 +201,14 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
                     mValid = false
                 }
 
+                mInitialRotation = 0.toDouble()
                 mStarted = false
             }
 
             MotionEvent.ACTION_POINTER_UP -> {
                 count -= 1
 
-                if (count == 2 && state !== UIGestureRecognizer.State.Failed) {
-
+                if (mDown && count == 2 && state !== UIGestureRecognizer.State.Failed) {
                     val pointerIndex = action and MotionEvent.ACTION_POINTER_INDEX_MASK shr MotionEvent.ACTION_POINTER_INDEX_SHIFT
                     val pointerId = event.getPointerId(pointerIndex)
 
@@ -223,13 +236,16 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
                     mValid = false
                 }
                 mStarted = false
+                mInitialRotation = 0.toDouble()
             }
 
             MotionEvent.ACTION_UP -> {
                 mValid = false
                 mStarted = false
-                mPreviousAngle = 0f
-                velocity = 0f
+                mDown = false
+                mPreviousAngle = 0.toDouble()
+                mInitialRotation = 0.toDouble()
+                velocity = 0.toDouble()
 
                 if (null != mPreviousEvent) {
                     mPreviousEvent!!.recycle()
@@ -252,10 +268,12 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
                 val nx2 = event.getX(event.findPointerIndex(mPtrID2))
                 val ny2 = event.getY(event.findPointerIndex(mPtrID2))
 
-                rotationInRadians = angleBetweenLines(x2, y2, x1, y1, nx2, ny2, nx1, ny1)
 
-                if (!mStarted) {
-                    if (Math.abs(rotationInRadians) > rotationThreshold) {
+                if (!mStarted && mDown) {
+                    mInitialRotation += angleBetweenLines(x2, y2, x1, y1, nx2, ny2, nx1, ny1)
+                    logMessage(Log.INFO, "mInitialRotation, $mInitialRotation, $rotationThreshold")
+
+                    if (Math.abs(mInitialRotation) > rotationThreshold) {
                         mStarted = true
 
                         if (delegate?.shouldBegin?.invoke(this)!!) {
@@ -266,7 +284,8 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
                             } else {
                                 when {
                                     requireFailureOf!!.state === UIGestureRecognizer.State.Failed -> fireActionEventIfCanRecognizeSimultaneously()
-                                    requireFailureOf!!.inState(UIGestureRecognizer.State.Began, UIGestureRecognizer.State.Ended, UIGestureRecognizer.State.Changed) -> state = UIGestureRecognizer.State.Failed
+                                    requireFailureOf!!.inState(UIGestureRecognizer.State.Began, UIGestureRecognizer.State.Ended, UIGestureRecognizer.State.Changed) -> state =
+                                            UIGestureRecognizer.State.Failed
                                     else -> {
                                         listenForOtherStateChanges()
                                         setBeginFiringEvents(false)
@@ -278,27 +297,29 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
                             state = UIGestureRecognizer.State.Failed
                         }
                     }
-                } else {
-                    if (state === UIGestureRecognizer.State.Began) {
+                } else if (mStarted && mDown) {
+                    rotationInRadians = angleBetweenLines(x2, y2, x1, y1, nx2, ny2, nx1, ny1)
+
+                    if (state == UIGestureRecognizer.State.Began) {
                         if (hasBeganFiringEvents()) {
                             state = UIGestureRecognizer.State.Changed
                             fireActionEvent()
                         }
-                    } else if (state === UIGestureRecognizer.State.Changed) {
+                    } else if (state == UIGestureRecognizer.State.Changed) {
                         state = UIGestureRecognizer.State.Changed
                         fireActionEvent()
                     }
                 }
 
-                if (null != mPreviousEvent) {
+                mPreviousEvent?.let {
                     val diff = Math.max(rotationInRadians, mPreviousAngle) - Math.min(rotationInRadians, mPreviousAngle)
-                    val time = event.eventTime - mPreviousEvent!!.eventTime
+                    val time = event.eventTime - it.eventTime
                     velocity = if (time > 0) {
                         1000 / time * diff
                     } else {
-                        0f
+                        0.toDouble()
                     }
-                    mPreviousEvent!!.recycle()
+                    it.recycle()
                 }
 
                 x1 = event.getX(event.findPointerIndex(mPtrID1))
@@ -316,8 +337,9 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
 
                 mValid = false
                 mStarted = false
-                mPreviousAngle = 0f
-                velocity = 0f
+                mDown = false
+                mPreviousAngle = 0.toDouble()
+                velocity = 0.toDouble()
 
                 if (null != mPreviousEvent) {
                     mPreviousEvent!!.recycle()
@@ -336,10 +358,17 @@ open class UIRotateGestureRecognizer(context: Context) : UIGestureRecognizer(con
         return cancelsTouchesInView
     }
 
-    private fun angleBetweenLines(fX: Float, fY: Float, sX: Float, sY: Float, nfX: Float, nfY: Float, nsX: Float, nsY: Float): Float {
-        val angle1 = Math.atan2((fY - sY).toDouble(), (fX - sX).toDouble()).toFloat()
-        val angle2 = Math.atan2((nfY - nsY).toDouble(), (nfX - nsX).toDouble()).toFloat()
-        return angle1 - angle2
+    private fun angleBetweenLines(fX: Float, fY: Float, sX: Float, sY: Float, nfX: Float, nfY: Float, nsX: Float,
+                                  nsY: Float): Double {
+        val angle1 = atan2((fY - sY), (fX - sX)).toDouble()
+        val angle2 = atan2((nfY - nsY), (nfX - nsX)).toDouble()
+        return if ((angle1 >= 0 && angle2 >= 0) || (angle1 < 0 && angle2 < 0)) {
+            angle2 - angle1
+        } else if (angle2 < 0) {
+            angle1 + angle2
+        } else {
+            angle2 + angle1
+        }
     }
 
     override fun removeMessages() {
