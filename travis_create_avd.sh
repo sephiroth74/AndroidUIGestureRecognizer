71c93c6e146f055aa93773d6184ea2d3783a6930
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2019-01-02 10:44:50 +0100

    travis

--- travis_create_avd.sh
+++ travis_create_avd.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 echo "TEST_TYPE=$TEST_TYPE"
 echo "EMULATOR_TAG=$EMULATOR_TAG"
 echo "EMULATOR_API=$EMULATOR_API"
 echo "ABI=$ABI"
 
+echo "Running android update sdk"
 echo y | android update sdk --no-ui --all --filter "android-$EMULATOR_API"
 android-update-sdk --components="sys-img-$ABI-$EMULATOR_TAG-$EMULATOR_API" --accept-licenses='android-sdk-license-[0-9a-f]{8}'
+
+echo "Running android list targets"
 android list targets
 # echo no | android create avd --force -n test -k $EMULATOR_TAG-$EMULATOR_API --abi $ABI --skin QVGA
+
+echo "Creating the emulator"
 echo no | $ANDROID_HOME/tools/bin/avdmanager create avd --force -n test -k "system-images;$EMULATOR_TAG-$EMULATOR_API;default;$ABI" -d pixel
+
+echo "Starting the emulator"
 emulator -avd test -no-audio -netfast -no-window &
 
 exit 0
