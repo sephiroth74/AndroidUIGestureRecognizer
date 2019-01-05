750f7eaeb0b04be2df13fe307e57c865c8182238
sephiroth74 <alessandro.crugnola@gmail.com>
2019-01-05 11:33:28 -0500

    test requireFailure

--- uigesturerecognizer/src/androidTest/java/it/sephiroth/android/library/uigestures/TestTapGesture.kt
+++ uigesturerecognizer/src/androidTest/java/it/sephiroth/android/library/uigestures/TestTapGesture.kt
@@ -128,6 +128,81 @@ class TestTapGesture : TestBaseClass() {
     }
 
     @Test
+    fun testTapMultiple() {
+        setTitle("Tap Tap")
+        val latch = CountDownLatch(1)
+
+        delegate.clear()
+        assertEquals(0, delegate.size())
+
+        val recognizer1 = UITapGestureRecognizer(context)
+        recognizer1.tag = "tap-1"
+
+        val recognizer2 = UITapGestureRecognizer(context)
+        recognizer2.tag = "tap-2"
+
+        recognizer1.actionListener = { it ->
+            activity.actionListener.invoke(it)
+            assertEquals(State.Ended, it.state)
+            latch.countDown()
+        }
+
+        recognizer2.actionListener = { it ->
+            fail()
+        }
+
+        delegate.addGestureRecognizer(recognizer1)
+        delegate.addGestureRecognizer(recognizer2)
+
+        delegate.shouldRecognizeSimultaneouslyWithGestureRecognizer = { it1: UIGestureRecognizer, it2: UIGestureRecognizer ->
+            Timber.d("it1 = $it1, it2 = $it2")
+            false
+        }
+
+        onView(ViewMatchers.withId(R.id.activity_main)).perform(ViewActions.click())
+
+        latch.await(2, TimeUnit.SECONDS)
+
+        assertEquals(0, latch.count)
+    }
+
+    @Test
+    fun testTapMultiple2() {
+        setTitle("Tap Tap")
+        val latch = CountDownLatch(2)
+
+        delegate.clear()
+        assertEquals(0, delegate.size())
+
+        val recognizer1 = UITapGestureRecognizer(context)
+        recognizer1.tag = "tap-1"
+
+        val recognizer2 = UITapGestureRecognizer(context)
+        recognizer2.tag = "tap-2"
+
+        recognizer1.actionListener = { it ->
+            activity.actionListener.invoke(it)
+            assertEquals(State.Ended, it.state)
+            latch.countDown()
+        }
+
+        recognizer2.actionListener = { it ->
+            activity.actionListener.invoke(it)
+            assertEquals(State.Ended, it.state)
+            latch.countDown()
+        }
+
+        delegate.addGestureRecognizer(recognizer1)
+        delegate.addGestureRecognizer(recognizer2)
+
+
+        onView(ViewMatchers.withId(R.id.activity_main)).perform(ViewActions.click())
+
+        latch.await(2, TimeUnit.SECONDS)
+        assertEquals(0, latch.count)
+    }
+
+    @Test
     fun testSingleTapFailShouldNotBegin() {
         setTitle("Tap")
         delegate.clear()
@@ -465,171 +540,41 @@ class TestTapGesture : TestBaseClass() {
         assertEquals(1, latch.count)
     }
 
-//
-//    @Test
-//    fun test_singleTap() {
-//        setTitle("Single Tap")
-//        val latch = CountDownLatch(1)
-//        val activity = activityTestRule.activity
-//        val delegate = activity.delegate
-//        assertNotNull(delegate)
-//        delegate.clear()
-//
-//        val recognizer = UITapGestureRecognizer(context)
-//        recognizer.tag = "single-tap"
-//        recognizer.touchesRequired = 1
-//        recognizer.tapsRequired = 1
-//        recognizer.actionListener = {
-//            activityTestRule.activity.actionListener.invoke(it)
-//            assertEquals(State.Ended, it.state)
-//            latch.countDown()
-//        }
-//        delegate.addGestureRecognizer(recognizer)
-//
-//        onView(ViewMatchers.withId(R.id.activity_main)).perform(ViewActions.click())
-//        latch.await(10, TimeUnit.SECONDS)
-//        assertEquals(0L, latch.count)
-//    }
-//
-//    @Test
-//    fun test_singleTap2Fingers() {
-//        setTitle("Single Tap 2 Fingers")
-//        val latch = CountDownLatch(1)
-//        val delegate = activityTestRule.activity.delegate
-//        assertNotNull(delegate)
-//        delegate.clear()
-//
-//        val recognizer = UITapGestureRecognizer(context)
-//        recognizer.tag = "single-tap"
-//        recognizer.touchesRequired = 2
-//        recognizer.tapsRequired = 1
-//        recognizer.tapTimeout = 400
-//
-//        recognizer.actionListener = {
-//            activityTestRule.activity.actionListener.invoke(it)
-//            assertEquals(State.Ended, it.state)
-//            latch.countDown()
-//        }
-//
-//        delegate.addGestureRecognizer(recognizer)
-//
-//        val pt1 = super.randomPointOnScreen()
-//        val pt2 = super.randomPointOnScreen()
-//
-//        val array = arrayListOf<Array<MotionEvent.PointerCoords>>()
-//        array.add(arrayOf(
-//                PointerCoordsBuilder.newBuilder().setSize(1f).setPressure(1f).setCoords(pt1.x.toFloat(), pt1.y.toFloat()).build(),
-//                PointerCoordsBuilder.newBuilder().setSize(1f).setPressure(1f).setCoords(pt2.x.toFloat(), pt2.y.toFloat()).build()
-//                         ))
-//
-//        interaction.performMultiPointerGesture(array.toTypedArray())
-//        latch.await(10, TimeUnit.SECONDS)
-//        assertEquals(0L, latch.count)
-//    }
-//
-//    @Test
-//    fun test_doubleTap() {
-//        setTitle("Double Tap")
-//        val latch = CountDownLatch(1)
-//        val activity = activityTestRule.activity
-//
-//        val delegate = activity.delegate
-//        delegate.clear()
-//
-//        val recognizer = UITapGestureRecognizer(context)
-//        recognizer.tag = "double-tap"
-//        recognizer.touchesRequired = 1
-//        recognizer.tapsRequired = 2
-//        recognizer.tapTimeout = 400
-//        recognizer.actionListener = {
-//            activityTestRule.activity.actionListener.invoke(it)
-//            assertEquals(State.Ended, it.state)
-//            latch.countDown()
-//        }
-//
-//        delegate.addGestureRecognizer(recognizer)
-//
-//        onView(ViewMatchers.withId(R.id.activity_main)).perform(ViewActions.doubleClick())
-//        latch.await(10, TimeUnit.SECONDS)
-//        assertEquals(0L, latch.count)
-//    }
-//
-//    @Test
-//    fun test_requireFailure() {
-//        setTitle("Tap Failure")
-//
-//        val latch = CountDownLatch(1)
-//        val activity = activityTestRule.activity
-//        val delegate = activity.delegate
-//
-//        delegate.clear()
-//        val recognizer1 = UITapGestureRecognizer(activity)
-//        recognizer1.tag = "single-tap"
-//        recognizer1.tapsRequired = 1
-//        recognizer1.tapTimeout = 300
-//
-//        val recognizer2 = UITapGestureRecognizer(activity)
-//        recognizer2.tag = "double-tap"
-//        recognizer2.tapsRequired = 2
-//        recognizer2.tapTimeout = 300
-//
-//        recognizer1.requireFailureOf = recognizer2
-//
-//        Log.e("test", "tap timeout: ${recognizer1.tapTimeout}")
-//
-//        recognizer1.actionListener = {
-//            Log.e("test", "recognizer1: $it")
-//            assertEquals(State.Ended, it.state)
-//            latch.countDown()
-//        }
-//
-//        recognizer2.actionListener = {
-//            Log.e("test", "recognizer2: $it")
-//            fail("Unexpected code!")
-//        }
-//
-//        recognizer1.stateListener = {
-//            Log.w("test", "recognizer1 state: $it")
-//        }
-//
-//        recognizer2.stateListener = {
-//            Log.w("test", "recognizer2 state: $it")
-//        }
-//
-//        delegate.addGestureRecognizer(recognizer1)
-//        delegate.addGestureRecognizer(recognizer2)
-//
-//        val bounds = mainView.visibleBounds
-//
-//        interaction.clickAndSync(bounds.centerX(), bounds.centerY(), 10)
-//        latch.await(10, TimeUnit.SECONDS)
-//        assertEquals(0L, latch.count)
-//    }
-//
-//
-//    @Test
-//    fun test_singleTapDisabled() {
-//        setTitle("Single Tap Disabled")
-//        val latch = CountDownLatch(1)
-//        val activity = activityTestRule.activity
-//        val delegate = activity.delegate
-//        assertNotNull(delegate)
-//        delegate.clear()
-//
-//        val recognizer = UITapGestureRecognizer(context)
-//        recognizer.tag = "single-tap"
-//        recognizer.actionListener = {
-//            activityTestRule.activity.actionListener.invoke(it)
-//            fail("no action expected!")
-//            latch.countDown()
-//        }
-//
-//        delegate.addGestureRecognizer(recognizer)
-//        delegate.isEnabled = false
-//
-//        mainView.click()
-//
-//        latch.await(5, TimeUnit.SECONDS)
-//        assertEquals(1L, latch.count)
-//    }
+    @Test
+    fun testRequireFailure() {
+        setTitle("Tap Failure")
+        val latch = CountDownLatch(1)
+
+        delegate.clear()
+
+        val recognizer1 = UITapGestureRecognizer(context)
+        recognizer1.tag = "tap-1"
+        recognizer1.tapsRequired = 2
+
+        val recognizer2 = UITapGestureRecognizer(context)
+        recognizer2.tag = "tap-2"
+        recognizer2.tapsRequired = 1
+
+        recognizer1.actionListener = { it ->
+            activity.actionListener.invoke(it)
+            fail()
+            latch.countDown()
+        }
+
+        recognizer2.actionListener = { it ->
+            activity.actionListener.invoke(it)
+            assertEquals(State.Ended, it.state)
+            latch.countDown()
+        }
+
+        recognizer2.requireFailureOf = recognizer1
+
+        delegate.addGestureRecognizer(recognizer1)
+        delegate.addGestureRecognizer(recognizer2)
+
+        onView(ViewMatchers.withId(R.id.activity_main)).perform(ViewActions.click())
+
+        latch.await(2, TimeUnit.SECONDS)
+        assertEquals(0, latch.count)
+    }
 }
