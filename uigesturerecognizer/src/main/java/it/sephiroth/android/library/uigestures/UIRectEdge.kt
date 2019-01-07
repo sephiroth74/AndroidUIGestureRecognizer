826edd22299f64be94292287390d7b31cf2c2556
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2019-01-07 17:08:58 -0500

    removed incorrect file headers

--- uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/UIRectEdge.kt
+++ uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/UIRectEdge.kt
@@ -1,21 +1,5 @@
 package it.sephiroth.android.library.uigestures
 
-//
-//  ADOBE CONFIDENTIAL
-//  __________________
-//
-//  Copyright 2017 Adobe
-//  All Rights Reserved.
-//
-//  NOTICE:  All information contained herein is, and remains
-//  the property of Adobe and its suppliers, if any. The intellectual
-//  and technical concepts contained herein are proprietary to Adobe
-//  and its suppliers and are protected by all applicable intellectual
-//  property laws, including trade secret and copyright laws.
-//  Dissemination of this information or reproduction of this material
-//  is strictly forbidden unless prior written permission is obtained
-//  from Adobe.
-//
 enum class UIRectEdge {
     LEFT, RIGTH, TOP, BOTTOM, NONE
 }
