e0e629fb340682cd0e1c895a8ba97b1747f778fd
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2019-01-07 17:07:57 -0500

    removed incorrect license information

--- uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/Geometry.kt
+++ uigesturerecognizer/src/main/java/it/sephiroth/android/library/uigestures/Geometry.kt
@@ -4,18 +4,6 @@ import android.graphics.Point
 import android.graphics.PointF
 import kotlin.math.sqrt
 
-/**
- * Copyright 2017 Adobe Systems Incorporated.  All rights reserved.
- * $Id$
- * $DateTime$
- * $Change$
- * $File$
- * $Revision$
- * $Author$
- */
-object Geometry {
-}
-
 fun PointF.distance(other: PointF): Float {
     return sqrt((other.y - y) * (other.y - y) + (other.x - x) * (other.x - x))
 }
